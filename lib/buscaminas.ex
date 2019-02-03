9bbbf1c0c187baa63d3be70d9ea7d5e8455d0604
berenice <bemesa21@gmail.com>
2019-02-03 16:52:33 -0600

    se anade buscaminas funcionando

---
+++ lib/buscaminas.ex
@@ -0,0 +1,46 @@
+defmodule Buscaminas do
+  def obtiene() do
+    {response, contents} = File.read("lib/minas.txt")
+    case response do
+      :ok -> transform(contents) |> busca() |> imprime()
+      _->IO.puts(response)
+    end
+  end
+
+  def transform(text) do 
+    trimed_text = text |> String.split("\n", trim: true)
+    [dimensions|lines] = trimed_text 
+    [dim1,dim2] = dimensions |> String.split()
+    Enum.map(lines, fn(l) -> String.graphemes(l) end) |> List.flatten
+  end
+
+  def busca(matrix) do
+    mtx2 =  List.duplicate(0, length(matrix))
+    minas = Enum.with_index(matrix) |> Enum.map(fn {e, idx} -> if e == "*" do idx end end) |> Enum.filter(fn a -> a != nil end)
+    solution = for n <- Enum.map(minas,fn(a) -> llena(mtx2,a,4) end) |> Enum.zip, do: Tuple.to_list(n) |> Enum.sum
+    Enum.chunk_every(solution,4) #|> Enum.each(File.write("lib/result.txt")) 
+  end  
+
+  def llena(array,position,ancho) do
+    my_positions = cond do
+      rem(position + 1,ancho) == 1 ->
+      [position - ancho, position - (ancho - 1),position + 1,position + ancho ,position + ancho + 1]
+      rem(position + 1,ancho) == 0 ->
+      [position - (ancho + 1),position - ancho,position - 1,position + (ancho - 1) ,position + ancho]
+      true ->  [position - ancho + 1,position - ancho, position - ancho - 1, position - 1, position + 1, position + ancho - 1 ,position + ancho ,position + ancho + 1]
+    end
+    my_positions = Enum.filter(my_positions,fn a -> a > 0 end)
+    Enum.each(my_positions,fn a -> IO.puts(a) end)
+    for n <- Enum.map(my_positions, fn(a) -> if a != "*" do List.update_at(array,a,&(&1 + 1)) end end) |> Enum.zip , do: Tuple.to_list(n) |> Enum.sum
+  end
+
+  def imprime(results) do 
+     {:ok, file} = File.open "lib/result.txt", [:write]
+     Enum.each(results,fn(a)->aux(a,file)end)  
+  end
+
+  def aux(line,file) do
+    Enum.each(line,fn(l)->IO.write(file,l) end)
+    IO.write(file,"\n")
+  end
+end
