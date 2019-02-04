a7f48005c286b5c385d76d66162ffaed55e11f3c
berenice <bemesa21@gmail.com>
2019-02-03 23:42:47 -0600

    buscaminas completo

--- lib/buscaminas.ex
+++ lib/buscaminas.ex
@@ -10,43 +10,48 @@ defmodule Buscaminas do
   def transform(text) do 
     trimed_text = text |> String.split("\n", trim: true)
     [dimensions|lines] = trimed_text 
-    [_dim1,dim2] = dimensions |> String.split() |> Enum.map(fn(a)->String.to_integer(a) end)
+    [_dim1,dim2] = dimensions |> String.split() |> Enum.map(fn(a) -> String.to_integer(a) end)
     array = Enum.map(lines, fn(l) -> String.graphemes(l) end) |> List.flatten
-    result = {_dim1,dim2,array}
+    {_dim1,dim2,array}
   end
 
   def busca({dim1,dim2,array}) do
     mtx2 =  List.duplicate(0, length(array))
     minas = Enum.with_index(array) 
-            |> Enum.map(fn {e, idx} -> if e == "*" do idx end end) 
-            |> Enum.filter(fn a -> a != nil end)
+    |> Enum.map(fn {e, idx} -> if e == "*" do idx end end) 
+    |> Enum.filter(fn a -> a != nil end)
     solution = for n <- Enum.map(minas,fn(a) -> llena(mtx2,a,dim1) end) 
-               |> Enum.zip, do: Tuple.to_list(n) 
-               |> Enum.sum
+    |> Enum.zip, do: Tuple.to_list(n) 
+    |> List.foldl(0, fn (x),  acc -> if is_integer(x) and is_integer(acc) do x + acc else "*" end end)
     Enum.chunk_every(solution,dim1) 
   end
 
   def llena(array,position,ancho) do
     my_positions = cond do
       rem(position + 1,ancho) == 1 ->
-      [position - ancho, position - (ancho - 1),position + 1,position + ancho ,position + ancho + 1]
-      rem(position + 1,ancho) == 0 ->
-      [position - (ancho + 1),position - ancho,position - 1,position + (ancho - 1) ,position + ancho]
-      true ->  [position - ancho + 1,position - ancho, position - ancho - 1, position - 1, position + 1, position + ancho - 1 ,position + ancho ,position + ancho + 1]
+        [position - ancho, position - (ancho - 1),position + 1,position + ancho ,position + ancho + 1]
+        rem(position + 1,ancho) == 0 ->
+          [position - (ancho + 1),position - ancho,position - 1,position + (ancho - 1) ,position + ancho]
+          true ->  [position - ancho + 1,position - ancho, position - ancho - 1, position - 1, position + 1, position + ancho - 1 ,position + ancho ,position + ancho + 1]
+        end
+        my_positions = Enum.filter(my_positions,fn a -> a > 0 end)
+        array = List.replace_at(array,position,"*")
+        for n <- Enum.map(my_positions, fn(a) -> if a != "*" do List.update_at(array,a,&(&1 + 1)) end end) 
+        |> Enum.zip , do: Tuple.to_list(n) 
+        |> List.foldl(0, fn (x),  acc -> if is_integer(x) do x + acc else "*" end end)
+      end
+
+      def imprime(results) do 
+        {response, file} = File.open "lib/result.txt", [:write]
+        case response do
+          :ok -> Enum.each(results,fn(a)->aux(a,file)end)  
+          _->IO.puts(response)
+        end   
+      end
+
+      def aux(line,file) do
+        Enum.each(line,fn(l)-> IO.write(file,l) end)
+        IO.write(file,"\n")
+      end
+
     end
-    my_positions = Enum.filter(my_positions,fn a -> a > 0 end)
-    for n <- Enum.map(my_positions, fn(a) -> if a != "*" do List.update_at(array,a,&(&1 + 1)) end end) 
-              |> Enum.zip , do: Tuple.to_list(n) 
-              |> Enum.sum
-  end
-
-  def imprime(results) do 
-     {:ok, file} = File.open "lib/result.txt", [:write]
-     Enum.each(results,fn(a)->aux(a,file)end)  
-  end
-
-  def aux(line,file) do
-    Enum.each(line,fn(l)->IO.write(file,l) end)
-    IO.write(file,"\n")
-  end
-end
