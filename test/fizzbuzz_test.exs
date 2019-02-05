3e8fac8c2ee37ec445b0f5029cebee2f4f0c4c9d
Agustín Ramos <zentimental.software@gmail.com>
2019-02-05 10:53:58 -0600

    Be more exhaustive with your tests

--- test/fizzbuzz_test.exs
+++ test/fizzbuzz_test.exs
@@ -2,7 +2,7 @@ defmodule FizzBuzzTest do
   use ExUnit.Case
   doctest FizzBuzz
 
-  [3, 6, 9, 12, 18]
+  [3, 6, 9, 12, 18, 21, 33, 36, 99]
   |> Enum.each(fn n ->
     @n n
     test "three multiples: #{@n}" do
@@ -10,7 +10,7 @@ defmodule FizzBuzzTest do
     end
   end)
 
-  [5, 10, 20, 25, 35]
+  [5, 10, 20, 25, 35, 40, 50, 55, 65, 80, 95]
   |> Enum.each(fn n ->
     @n n
     test "five multiples: #{@n}" do
@@ -19,7 +19,7 @@ defmodule FizzBuzzTest do
   end)
 
 
-  [15, 30, 60]
+  [15, 30, 45, 60, 75, 90]
   |> Enum.each(fn n ->
     @n n
     test "both: #{@n}" do
@@ -28,6 +28,6 @@ defmodule FizzBuzzTest do
   end)
 
   test "one_number" do
-    assert is_integer(FizzBuzz.validate(1)) 
+    assert is_integer(FizzBuzz.validate(1))
   end
 end
