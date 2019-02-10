c53bcfcbcc596bbd145cf036dfeed75fd55ac37d
berenice <bemesa21@gmail.com>
2019-02-10 17:48:34 -0600

    se cambian registros a forma de mapa

--- lib/OpeningTheTuringLock.ex
+++ lib/OpeningTheTuringLock.ex
@@ -5,84 +5,41 @@ defmodule OpeningTheTuringLock do
     :ok -> 
       text 
       |> String.split("\n", trim: true)
-      |> execute_instructions(0 ,0 ,0)
-
+      |> execute_instructions(%{"a" => 0, "b" => 0}, 0)
     _ -> IO.puts(status)
     end
   end
 
-  def execute_instructions(instruction_list, a, b, pos) when pos >= length(instruction_list) or pos < 0 do
-    %{"a" => a ,"b" => b}
+  def execute_instructions(instruction_list, registers, pos) when pos >= length(instruction_list) or pos < 0 do
+    registers
   end
 
-  def execute_instructions(instruction_list, a, b, pos) do  
+  def execute_instructions(instruction_list, registers, pos) do  
     case List.to_tuple(String.split(Enum.at(instruction_list, pos),[" ", ","],trim: true)) do
-      {"hlf", register} -> 
-        case register do
-          "a" -> execute_instructions(instruction_list ,Integer.floor_div(a,2) ,b, pos + 1 ) 
-          "b" -> execute_instructions(instruction_list ,a ,Integer.floor_div(b,2), pos + 1 ) 
-          _ -> IO.puts(:error) 
-        end
-        
-      {"tpl", register} -> 
-        case register do 
-          "a" -> execute_instructions(instruction_list ,a * 3 ,b, pos + 1 ) 
-          "b" -> execute_instructions(instruction_list ,a ,b * 3, pos + 1 ) 
-          _ -> IO.puts(:error)
-        end
-
-      {"inc", register} -> 
-        case register do 
-          "a" -> execute_instructions(instruction_list ,a + 1 ,b, pos + 1 ) 
-          "b" -> execute_instructions(instruction_list ,a ,b + 1, pos + 1 ) 
-          _ -> IO.puts(:error)
-        end
-
-      {"jmp", offset} -> 
-        <<operator :: binary-size(1)>> <> num = offset
-        case operator do
-          "+" -> execute_instructions(instruction_list ,a ,b, pos + String.to_integer(num)) 
-          "-" -> execute_instructions(instruction_list ,a ,b, pos - String.to_integer(num)) 
-          _ -> :error
-        end
-
+      {"inc", register} -> execute_instructions(instruction_list, Map.update!(registers, register, &(&1 + 1)), pos + 1 )
+      {"hlf", register} -> execute_instructions(instruction_list, Map.update!(registers, register, &(Integer.floor_div(&1,2))), pos + 1 )
+      {"tpl", register} -> execute_instructions(instruction_list, Map.update!(registers, register, &(&1 * 3)), pos + 1 )
+      {"jmp", offset} -> execute_instructions(instruction_list, registers, new_position(pos, offset)) 
       {"jie", register,offset} -> 
-        eval_result = 
-          case register do
-            "a" -> rem(a, 2) == 0
-            "b" -> rem(b, 2) == 0
-          end 
-
-        case eval_result do
-          true -> 
-            <<operator :: binary-size(1)>> <> num = offset
-            case operator do
-              "+" -> execute_instructions(instruction_list ,a ,b, pos + String.to_integer(num)) 
-              "-" -> execute_instructions(instruction_list ,a ,b, pos - String.to_integer(num)) 
-              _ -> :error
-            end
-          false -> execute_instructions(instruction_list ,a ,b, pos + 1) 
+        case rem(Map.get(registers, register), 2) do
+          0 -> execute_instructions(instruction_list ,registers, new_position(pos, offset)) 
+          _ -> execute_instructions(instruction_list ,registers, pos + 1) 
         end
-
       {"jio", register,offset} -> 
-        eval_result = 
-          case register do
-            "a" -> a == 1
-            "b" -> b == 1
-          end 
-
-        case eval_result do
-          true -> 
-            <<operator :: binary-size(1)>> <> num = offset
-            case operator do
-              "+" -> execute_instructions(instruction_list ,a ,b, pos + String.to_integer(num)) 
-              "-" -> execute_instructions(instruction_list ,a ,b, pos - String.to_integer(num)) 
-              _ -> :error
-            end
-          false -> execute_instructions(instruction_list ,a ,b, pos + 1) 
+        case Map.get(registers, register) == 1 do
+          true -> execute_instructions(instruction_list ,registers, new_position(pos, offset)) 
+          false -> execute_instructions(instruction_list ,registers, pos + 1) 
         end
+      _ -> registers
+    end
+  end
 
-      _ -> %{"a" => a ,"b" => b}   #instruction not found
+  def new_position(pos,offset) do
+    <<operator :: binary-size(1)>> <> num = offset
+    case operator do
+      "+" -> pos + String.to_integer(num)
+      "-" -> pos - String.to_integer(num)
+      _ -> :error
     end
   end
 end
